6c08244943b4d8a7e64ba60f53cfa9ecc613029b
Uli Hansen <ulrich.hansen@acentic.com>
2019-03-15 10:44:22 +0100

    first commit

---
+++ src/main/java/com/hanulhan/tpvWebService/TpvWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hanulhan.tpvWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpvWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpvWebServiceApplication.class, args);
+	}
+
+}
